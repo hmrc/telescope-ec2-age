0a12b09443b70f23eb53a0ffba91a636dd85a38d
Samuel Allwright <71259281+samallwright@users.noreply.github.com>
2021-01-18 17:13:13 +0000

    TEL-2221 WIP Fixed instance catch exception for missing instanceID

--- telemetry/telescope_ec2_age/instance_uptime.py
+++ telemetry/telescope_ec2_age/instance_uptime.py
@@ -33,11 +33,35 @@ def describe_instances(instance_list):
             for instance in reservation["Instances"]:
                 launch_time = instance["LaunchTime"]
                 instance_id_name = instance["InstanceId"]
+                # print(instance_id_name)
                 instance_launch_times[instance_id_name] = launch_time
+                # print(launch_time)
+        print("success")
         return instance_launch_times
+
+##this is og that worked???? explain why middle doesnt
+    # except ClientError as e:
+    #     instance_launch_times = {}
+    #     for row in instance_list:
+    #         instance_launch_times[row] = None
+    #         print(instance_launch_times[row])
+    #     logger.error(str(e))
+    #     return instance_launch_times
+
+#this is attempt 1
     except ClientError as e:
-        logger.error(str(e))
-        return None
+        instance_launch_times = {}
+        print(instance_list)
+        for row in instance_list:
+            launch_time = describe_try(row)
+            print('*************&&&&&&&&&&&&&^^^^^^^^^^^^^ launchtime *************&&&&&&&&&&&&&^^^^^^^^^^^^^')
+            print(launch_time)
+            instance_launch_times[row] = launch_time
+            print('*************&&&&&&&&&&&&&^^^^^^^^^^^^^ instance row *************&&&&&&&&&&&&&^^^^^^^^^^^^^')
+            print(instance_launch_times[row])
+        # logger.error(str(e))
+        return instance_launch_times
+
     except Exception as e:
         logger.error(str(e))
         return None
@@ -46,17 +70,44 @@ def describe_instances(instance_list):
         return None
 
 
+def describe_try(instance):
+        print("describe try()" + str(instance)+ "\n")
+        try:
+            response = ec2_client.describe_instances(
+                InstanceIds=[instance]
+            )
+            ###response works! instanceIds needs [] as only takes list
+            print('response *********************************&&&&&&&&&&&&&^^^^^^^^^^^^^')
+            # print(response)
+            for reservation in response["Reservations"]:
+                for instance in reservation["Instances"]:
+                    print(instance)
+                    launch_time = instance["LaunchTime"]
+                    print("\n")
+                    print(launch_time)
+                    return launch_time
+
+                    ####WE CAN OBTAIN LAUNCH TIME WHAT SO WHAT IS THE ISSUE IM SO CONFUSED NEED TO INVESTIGATE TOMORROW
+                    #####SUCCESSSSSS??????
+            # print('try launch time' + launch_time)
+            # return launch_time
+        except:
+            logger.error("Unexpected error:", sys.exc_info()[0])
+            return None
+
+
 def instance_time(instance_launch_times):
     if instance_launch_times is None:
         return instance_launch_times
     instance_uptime = {}
     for instance_id, launch_time in instance_launch_times.items():
-        timedelta = datetime.now(launch_time.tzinfo) - launch_time
-        instance_uptime[instance_id] = timedelta.total_seconds()
+        if launch_time is not None:
+            timedelta = datetime.now(launch_time.tzinfo) - launch_time
+            instance_uptime[instance_id] = timedelta.total_seconds()
 
-        logger.debug('instance id: ' + str(instance_id))
-        logger.debug('intance Timestamp: creationDate: ' + str(launch_time))
-        logger.debug('instance Timestamp: delta: ' + str(timedelta.total_seconds()))
+            logger.debug('instance id: ' + str(instance_id))
+            logger.debug('intance Timestamp: creationDate: ' + str(launch_time))
+            logger.debug('instance Timestamp: delta: ' + str(timedelta.total_seconds()))
     return instance_uptime
 
 
@@ -65,5 +116,12 @@ def handler():
     asg_instance_uptime = {}
     for asg_name, list_of_instances in describe_asg().items():
         logger.debug('asg name:' + str(asg_name))
+        print('\n' + asg_name)
+        # print(list_of_instances)
         asg_instance_uptime[asg_name] = instance_time(describe_instances(list_of_instances))
     return asg_instance_uptime
+
+handler()
+# for a, b in handler().items():
+#     print(a)
+#     print(b)
